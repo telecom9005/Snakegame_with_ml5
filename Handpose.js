123f28be743e224f50bd9c46a120643aa08a1aed
최연우 <50083524+wafla@users.noreply.github.com>
2022-05-19 14:28:39 +0900

    Update Handpose.js

--- Handpose.js
+++ Handpose.js
@@ -1,52 +1,34 @@
 //https://wikidocs.net/103185
 //<script src="https://unpkg.com/ml5@latest/dist/ml5.min.js"></script>
-let img;
 let handpose;
-let fingers=[];
-let keypointX = [];
-let keypointY = [];
-
-function preload(){
-  img = loadImage('data/paper1.png');
-}
+let video;
+let predictions = [];
 
 function setup() {
-  createCanvas(img.width, img.height);
-  handpose = ml5.handpose(modelReady);
-  handpose.on('predict', gotResult);
-}
-
-function modelReady(){
-  handpose.predict(img);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+  handpose = ml5.handpose(video);
+  handpose.on("predict", gotResult);
 }
 
 function gotResult(results){
-  fingers = results[0].landmarks;  
-  console.log(results);  
+  predictions = results;
 }
 
-function draw(){
-  image(img, 0, 0);
-  fill('#FFFF00');
-  stroke('#FF0000');
-  strokeWeight(3);
-  for(let i = 0; i < fingers.length; i++){
-    keypointX[i]=round(fingers[i][0]);
-    keypointY[i]=round(fingers[i][1]);
-    ellipse(keypointX[i], keypointY[i],10);
-  }
+function draw() {
+  image(video, 0, 0, width, height);
+  drawKeypoints();
+}
 
-  stroke('#FFFF00');
-  strokeWeight(3);
-  line(keypointX[0],keypointY[0],keypointX[1],keypointY[1]);    
-  line(keypointX[2],keypointY[2],keypointX[5],keypointY[5]);  
-  line(keypointX[5],keypointY[5],keypointX[9],keypointY[9]);    
-  line(keypointX[9],keypointY[9],keypointX[13],keypointY[13]);
-  line(keypointX[13],keypointY[13],keypointX[17],keypointY[17]);
-  line(keypointX[17],keypointY[17],keypointX[0],keypointY[0]);        
-  for(let i=0; i<5; i++){
-    for(let j=0;j<3;j++){
-      line(round(keypointX[j+1+i*4]), round(keypointY[j+1+i*4]), round(keypointX[j+2+i*4]), round(keypointY[j+2+i*4]));
+function drawKeypoints() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const prediction = predictions[i];
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
     }
   }
 }
